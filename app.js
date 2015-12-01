fc5622c45b180dae148354427c9e556afdac773a
hochul1005@naver.com <hochul1005@naver.com>
2015-12-01 09:54:50 +0900

    modify_mainPage

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var methodOverride = require('method-override');
 var flash = require('connect-flash');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var configAuth = require('./config/auth');
 
-var routes = require('./routes/index');
+var routes = require('./routes/index'),
+    users = require('./routes/users');
+var routeAuth = require('./routes/auth');
 
 var app = express();
 
@@ -20,13 +25,17 @@ if (app.get('env') === 'development') {
 }
 app.locals.moment = require('moment');
 
+// mongodb connect
+mongoose.connect('mongodb://hochul1005:k27101329@ds045064.mongolab.com:45064/webpro');
+mongoose.connection.on('error', console.log);
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(methodOverride('_method'));
+app.use(methodOverride('_method', {methods: ['POST', 'GET']}));
 
 app.use(session({
   resave: true,
@@ -34,10 +43,23 @@ app.use(session({
   secret: 'long-long-long-secret-string-1313513tefgwdsvbjkvasd'
 }));
 app.use(flash());
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower',  express.static(path.join(__dirname, '/bower')));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.flashMessages = req.flash();
+  next();
+});
+
+configAuth(passport);
+
 app.use('/', routes);
+app.use('/users', users);
+routeAuth(app, passport);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ router.get('/', function(req, res, next){
   res.render('index',{messages: req.flash()});
 });
 
+router.get('/signin', function(req, res, next) {
+  res.render('signin');
+});
+
 module.exports = router;
